40a61ffaa06939c0f2cb4b2a06d0a2ada3b8db9c
hye-yeong lee <i-e@yukoyuko.co.jp>
2022-11-11 16:03:26 +0900

    feat: useRef사용해보기

--- src/InputSample.js
+++ src/InputSample.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useRef } from "react";
 
 function InputSample(){
   const [data, setData] = useState({
@@ -6,6 +6,7 @@ function InputSample(){
     nickname: ''
   })
   const {name, nickname} = data
+  const nameInput = useRef()
 
   const handleChange = (e) => {
     const {value, name} = e.target
@@ -17,13 +18,15 @@ function InputSample(){
   }
 
 
-  const inputReset = () => setData({
+  const inputReset = () =>{ setData({
     name: '',
     nickname:''
   })
+  nameInput.current.focus()
+}
   return (
   <div>
-    <input type="text" onChange={handleChange} value={name} name="name" placeholder={"이름을 입력해주세요"}/>
+    <input type="text" onChange={handleChange} value={name} name="name" placeholder={"이름을 입력해주세요"} ref={nameInput}/>
     <input type="text" onChange={handleChange} value={nickname} name="nickname" placeholder={"닉네임을 입력해주세요"}/>
     <button onClick={inputReset}>Reset</button>
     <br/>
